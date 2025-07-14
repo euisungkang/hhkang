63cd0dfecf29e8310575a1f4dc04c1713952e2a0
euisungkang <euisungkang@gmail.com>
2025-07-14 17:46:54 -0500

    feat: favicons and SEO

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,13 +6,16 @@
 	import hhkangCutout from '$lib/media/HHKang Cutout.webp';
 	import hhkangBG from '$lib/media/HHKang BG.webp';
 	import hhkangSteam from '$lib/media/HHKang Steam Cutout.webp';
-	import { onMount, onDestroy } from 'svelte';
+	import { onMount, onDestroy, tick } from 'svelte';
 	import Tabs from '$lib/components/overlay/Tabs.svelte';
 	import TitleWords from '$lib/components/about/TitleWords.svelte';
 	import Description1 from '$lib/components/about/Description1.svelte';
 	import Description2 from '$lib/components/about/Description2.svelte';
 	import Description3 from '$lib/components/about/Description3.svelte';
 
+	const preloadList = [hhkangCutout, hhkangBG, hhkangSteam];
+
+	let visible: boolean = $state(false);
 	let innerHeight: number = $state(0);
 	const textColor: string = '#e6e6e6';
 
@@ -23,12 +26,9 @@
 
 	const page1Lines: Array<string> = ['HUMANITIES', 'SCIENCE', 'KOREA'];
 	const page2Lines: Array<string> = ['MATERIAL_DESIGN'];
-	const page2Description: string = 'TESTING';
 	let page2Load: boolean = $state(false);
 	const page3Lines: Array<string> = ['SUNDIALS'];
 	let page3Load: boolean = $state(false);
-
-	// $inspect(scrollY, innerHeight, progress);
 
 	$effect(() => {
 		if (scrollY == innerHeight) {
@@ -40,7 +40,21 @@
 		}
 	});
 
-	onMount(() => {
+	async function preload(srcs: Array<string>) {
+		await Promise.all(
+			srcs.map(
+				(src) =>
+					new Promise((resolve, reject) => {
+						const img = new Image();
+						img.onload = resolve;
+						img.onerror = reject;
+						img.src = src;
+					})
+			)
+		);
+	}
+
+	onMount(async () => {
 		window.scrollTo(0, 0);
 		lenis = new Lenis({
 			autoRaf: true
@@ -49,6 +63,10 @@
 			scrollY = e.animatedScroll;
 			progress = e.progress;
 		});
+		await preload(preloadList);
+		visible = true;
+		await tick();
+
 		// snap = new Snap(lenis, {
 		// 	type: 'proximity'
 		// });
@@ -62,51 +80,54 @@
 
 <svelte:window bind:innerHeight />
 
-<Logo overlayColor={textColor} sticky={true} {scrollY} />
-<Tabs overlayColor={textColor} />
+{#if visible}
+	<Logo overlayColor={textColor} sticky={true} {scrollY} />
+	<Tabs overlayColor={textColor} />
+	<div
+		data-scroll-container
+		class="dark min-h-screen flex flex-col w-[100%] bg-[#121212]"
+		style:color={textColor}
+	>
+		<div class="max-h-screen w-full overflow-hidden relative">
+			<figure class="w-full">
+				<img src={hhkangBG} alt="HHKang BG" class="object-cover w-full" />
+			</figure>
 
-<div
-	data-scroll-container
-	class="dark min-h-screen flex flex-col w-[100%] bg-[#121212]"
-	style:color={textColor}
->
-	<div class="max-h-screen w-full overflow-hidden relative">
-		<figure class="w-full">
-			<img src={hhkangBG} alt="HHKang BG" class="object-cover w-full" />
-		</figure>
+			<figure
+				class="absolute -bottom-[16vh] left-0 w-full"
+				style="transform: translateY({scrollY * 0.2}px);"
+			>
+				<img src={hhkangCutout} alt="HHKang Cutout" class="w-full" />
+			</figure>
 
-		<figure
-			class="absolute -bottom-[16vh] left-0 w-full"
-			style="transform: translateY({scrollY * 0.2}px);"
-		>
-			<img src={hhkangCutout} alt="HHKang Cutout" class="w-full" />
-		</figure>
-
-		<figure
-			class="absolute -bottom-[16vh] right-0 w-full"
-			style="transform: translateY({scrollY * 0.4}px);"
-		>
-			<img src={hhkangSteam} alt="HHKang Steam" class="w-full" />
-		</figure>
-	</div>
-
-	<TitleWords {scrollY} lines={page1Lines} load={progress <= 0.05} />
-
-	<div class="h-screen w-full bg-[#e6e6e6] relative">
-		<Description1 overlayColor={'#121212'} />
-	</div>
-
-	{#if page2Load}
-		<div class="h-screen w-full bg-[#121212] text-[#e6e6e6] relative">
-			<Description2 overlayColor={'#e6e6e6'} />
-			<TitleWords lines={page2Lines} delay={1000} />
+			<figure
+				class="absolute -bottom-[16vh] right-0 w-full"
+				style="transform: translateY({scrollY * 0.4}px);"
+			>
+				<img src={hhkangSteam} alt="HHKang Steam" class="w-full" />
+			</figure>
 		</div>
-	{/if}
 
-	{#if page3Load}
-		<div class="h-screen w-full bg-black text-[#e6e6e6] relative">
-			<Description3 overlayColor={'#e6e6e6'} />
-			<TitleWords lines={page3Lines} delay={1000} />
+		<TitleWords {scrollY} lines={page1Lines} delay={100} load={progress <= 0.05} />
+
+		<div class="h-screen w-full bg-[#e6e6e6] relative">
+			<Description1 overlayColor={'#121212'} />
 		</div>
-	{/if}
-</div>
+
+		{#if page2Load}
+			<div class="h-screen w-full bg-[#121212] text-[#e6e6e6] relative">
+				<Description2 overlayColor={'#e6e6e6'} />
+				<TitleWords lines={page2Lines} delay={1000} />
+			</div>
+		{/if}
+
+		{#if page3Load}
+			<div class="h-screen w-full bg-black text-[#e6e6e6] relative">
+				<Description3 overlayColor={'#e6e6e6'} />
+				<TitleWords lines={page3Lines} delay={1000} />
+			</div>
+		{/if}
+	</div>
+{:else}
+	<div class="h-screen w-screen bg-gray-700"></div>
+{/if}
